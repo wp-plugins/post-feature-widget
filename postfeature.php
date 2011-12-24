480f9698e05fd3957b35eabbfeed4353f1ab4052
tepelstreel <tepelstreel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-24 23:17:06 +0000

    tagging pfw 3.0
    git-svn-id: https://plugins.svn.wordpress.org/post-feature-widget/tags/3.0@480301 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- postfeature.php
+++ postfeature.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Featured Post Widget
 Plugin URI: http://wasistlos.waldemarstoffel.com/plugins-fur-wordpress/featured-post-widget
-Description: Featured Post Widget is yet another plugin to make your blog a bit more newspaper-like. Just by entering the ID, you can put a post in the 'featured' area and display thumbnail, headline, excerpt or all three of them (if available) in the fully customizable widget.
-Version: 1.9
+Description: Featured Post Widget is yet another plugin to make your blog a bit more newspaper-like. Just by choosing a post from a dropdown, you can put it in the 'featured' area and display thumbnail, headline, excerpt or all three of them (if available) in the fully customizable widget.
+Version: 3.0
 Author: Waldemar Stoffel
 Author URI: http://www.waldemarstoffel.com
 License: GPL3
@@ -33,11 +33,10 @@ if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) { die("Sorr
 
 /* attach JavaScript file for textarea reszing */
 
-$fpw_path = WP_CONTENT_URL.'/plugins/'.plugin_basename(dirname(__FILE__)).'/';
-
 function fpw_js_sheet() {
-   global $fpw_path;
-   wp_enqueue_script('ta-resize-script', $fpw_path.'ta-expander.js', false, false, true);
+   
+   wp_enqueue_script('ta-expander-script', plugins_url('ta-expander.js', __FILE__), false, false, true);
+
 }
 
 add_action('admin_print_scripts-widgets.php', 'fpw_js_sheet');
@@ -63,7 +62,7 @@ class Featured_Post_Widget extends WP_Widget {
  
  function Featured_Post_Widget() {
 	 
-	 $widget_opts = array( 'description' => __('You can feature a certain post in this widget and display it, where and however you want, in your widget areas. The ID of a backup post can be given to avoid dubble content.', 'postfeature') );
+	 $widget_opts = array( 'description' => __('You can feature a certain post in this widget and display it, where and however you want, in your widget areas. A backup post can be given to avoid dubble content.', 'postfeature') );
 	 $control_opts = array( 'width' => 400 );
 
 	 
@@ -77,38 +76,19 @@ function form($instance) {
 	$image = esc_attr($instance['image']);
 	$article = esc_attr($instance['article']);
 	$backup = esc_attr($instance['backup']);
+	$class = esc_attr($instance['class']);
+	$myclass = esc_attr($instance['myclass']);
 	$width = esc_attr($instance['width']);
 	$subtitle = esc_attr($instance['subtitle']);	
 	$excerpt = esc_attr($instance['excerpt']);
+	$noshorts = esc_attr($instance['noshorts']);
+	$readmore = esc_attr($instance['readmore']);
+	$rmtext = esc_attr($instance['rmtext']);
 	$style = esc_attr($instance['style']);
 	
-	if (empty($style)) {
-		
-		$style_height=25;
-	
-	}
-	
-	else {
-		
-		$fpw_elements=str_replace(array("\r\n", "\n", "\r"), '|', $style);
-		$style_height=count(explode('|', $fpw_elements))*23;
-		
-	}
-	
-	if (empty($excerpt)) {
-		
-		$excerpt_height=25;
-	
-	}
-	
-	else {
-		
-		$fpw_elements=str_replace(array("\r\n", "\n", "\r"), '|', $excerpt);
-		$excerp_height=count(explode('|', $fpw_elements))*23;
-		
-	}
- 
- ?>
+	$features = get_posts('numberposts=-1');
+
+?>
  
 <p>
  <label for="<?php echo $this->get_field_id('title'); ?>">
@@ -117,20 +97,45 @@ function form($instance) {
  </label>
 </p>
 <p>
- <label for="<?php echo $this->get_field_id('article'); ?>">
- <?php _e('Give here the ID of the post, you want to appear in the widget:', 'postfeature'); ?>
- <input size="6" id="<?php echo $this->get_field_id('article'); ?>" name="<?php echo $this->get_field_name('article'); ?>" type="text" value="<?php echo $article; ?>" />
+  <label for="<?php echo $this->get_field_id( 'article' ); ?>"><?php _e('Choose here the post, you want to appear in the widget.', 'postfeature'); ?></label>
+  <select id="<?php echo $this->get_field_id( 'article' ); ?>" name="<?php echo $this->get_field_name( 'article' ); ?>" class="widefat" style="width:100%;">
+  <?php
+  if (empty($article)) echo '<option value="">'.__('Take a random post', 'postfeature').'</option>';  
+    foreach ( $features as $feature ) {
+      $selected = ( $feature->ID == $article ) ? 'selected="selected"' : '' ;
+      $option = '<option value="'.$feature->ID.'" '.$selected.' >'.$feature->post_title.'</option>';
+      echo $option;
+    }
+  ?>
+  </select>
+</p>
+<p>
+  <label for="<?php echo $this->get_field_id( 'backup' ); ?>"><?php _e('Choose here the backup post. It will appear, when a single post page shows the featured article.', 'postfeature'); ?></label>
+  <select id="<?php echo $this->get_field_id( 'backup' ); ?>" name="<?php echo $this->get_field_name( 'backup' ); ?>" class="widefat" style="width:100%;">
+  <?php
+  if (empty($article)) echo '<option value="">'.__('Take a random post', 'postfeature').'</option>';  
+    foreach ( $features as $feature ) {
+      $selected = ( $feature->ID == $backup ) ? 'selected="selected"' : '' ;
+      $option = '<option value="'.$feature->ID.'" '.$selected.' >'.$feature->post_title.'</option>';
+      echo $option;
+    }
+  ?>
+  </select>
+</p>
+<p>
+ <label for="<?php echo $this->get_field_id('class'); ?>">
+ <input id="<?php echo $this->get_field_id('class'); ?>" name="<?php echo $this->get_field_name('class'); ?>" <?php if(!empty($class)) echo "checked=\"checked\""; ?> type="checkbox" />&nbsp;<?php _e('I want to style the first paragraph in this widget with my own class.', 'postfeature'); ?>
  </label>
 </p>
 <p>
- <label for="<?php echo $this->get_field_id('backup'); ?>">
- <?php _e('Give here the ID of the backup post, it will appear, when a single post page shows the featured article:', 'postfeature'); ?>
- <input size="6" id="<?php echo $this->get_field_id('backup'); ?>" name="<?php echo $this->get_field_name('backup'); ?>" type="text" value="<?php echo $backup; ?>" />
+ <label for="<?php echo $this->get_field_id('myclass'); ?>">
+ <?php _e('Write here the name of your class:', 'postfeature'); ?>
+ <input class="widefat" id="<?php echo $this->get_field_id('myclass'); ?>" name="<?php echo $this->get_field_name('myclass'); ?>" type="text" value="<?php echo $myclass; ?>" />
  </label>
 </p>
 <p>
  <label for="<?php echo $this->get_field_id('image'); ?>">
- <input id="<?php echo $this->get_field_id('image'); ?>" name="<?php echo $this->get_field_name('image'); ?>" <?php if(!empty($image)) {echo "checked=\"checked\""; } ?> type="checkbox" />&nbsp;<?php _e('Check to get the first image of the post as thumbnail.', 'postfeature'); ?>
+ <input id="<?php echo $this->get_field_id('image'); ?>" name="<?php echo $this->get_field_name('image'); ?>" <?php if(!empty($image)) echo "checked=\"checked\""; ?> type="checkbox" />&nbsp;<?php _e('Check to get the first image of the post as thumbnail.', 'postfeature'); ?>
  </label>
 </p>
 <p>
@@ -141,26 +146,50 @@ function form($instance) {
 </p>
 <p>
  <label for="<?php echo $this->get_field_id('thumb'); ?>">
- <input id="<?php echo $this->get_field_id('thumb'); ?>" name="<?php echo $this->get_field_name('thumb'); ?>" <?php if(!empty($thumb)) {echo "checked=\"checked\""; } ?> type="checkbox" />&nbsp;<?php _e('Check to <strong>not</strong> display the thumbnail of the post.', 'postfeature'); ?>
+ <input id="<?php echo $this->get_field_id('thumb'); ?>" name="<?php echo $this->get_field_name('thumb'); ?>" <?php if(!empty($thumb)) echo "checked=\"checked\""; ?> type="checkbox" />&nbsp;<?php _e('Check to <strong>not</strong> display the thumbnail of the post.', 'postfeature'); ?>
  </label>
 </p>
 <p>
  <label for="<?php echo $this->get_field_id('subtitle'); ?>">
- <input id="<?php echo $this->get_field_id('subtitle'); ?>" name="<?php echo $this->get_field_name('subtitle'); ?>" <?php if(!empty($subtitle)) {echo "checked=\"checked\""; } ?> type="checkbox" />&nbsp;<?php _e('Check to display the title of the post <strong>under</strong> the thumbnail (it is above by default).', 'postfeature'); ?>
+ <input id="<?php echo $this->get_field_id('subtitle'); ?>" name="<?php echo $this->get_field_name('subtitle'); ?>" <?php if(!empty($subtitle)) echo "checked=\"checked\""; ?> type="checkbox" />&nbsp;<?php _e('Check to display the title of the post <strong>under</strong> the thumbnail (it is above by default).', 'postfeature'); ?>
+ </label>
+</p>
+<p>
+ <label for="<?php echo $this->get_field_id('noshorts'); ?>">
+ <input id="<?php echo $this->get_field_id('noshorts'); ?>" name="<?php echo $this->get_field_name('noshorts'); ?>" <?php if(!empty($noshorts)) echo "checked=\"checked\""; ?> type="checkbox" />&nbsp;<?php _e('Check to suppress shortcodes in the widget (in case the content is showing).', 'postfeature'); ?>
  </label>
 </p>
 <p>
  <label for="<?php echo $this->get_field_id('excerpt'); ?>">
  <?php _e('If the excerpt of the post is not defined, by default the first 3 sentences of the post are showed. You can enter your own excerpt here, if you want.', 'postfeature'); ?>
- <textarea class="widefat expand<?php echo $excerpt_height; ?>-1000" id="<?php echo $this->get_field_id('excerpt'); ?>" name="<?php echo $this->get_field_name('excerpt'); ?>"><?php echo $excerpt; ?></textarea>
+ <textarea class="widefat" id="<?php echo $this->get_field_id('excerpt'); ?>" name="<?php echo $this->get_field_name('excerpt'); ?>"><?php echo $excerpt; ?></textarea>
+ </label>
+</p>
+<p>
+ <label for="<?php echo $this->get_field_id('readmore'); ?>">
+ <input id="<?php echo $this->get_field_id('readmore'); ?>" name="<?php echo $this->get_field_name('readmore'); ?>" <?php if(!empty($readmore)) echo "checked=\"checked\""; ?> type="checkbox" />&nbsp;<?php _e('Check to have an additional &#39;read more&#39; link at the end of the excerpt.', 'postfeature'); ?>
+ </label>
+</p>
+<p>
+ <label for="<?php echo $this->get_field_id('rmtext'); ?>">
+ <?php _e('Write here some text for the &#39;read more&#39; link. By default, it is [...]:', 'postfeature'); ?>
+ <input class="widefat" id="<?php echo $this->get_field_id('rmtext'); ?>" name="<?php echo $this->get_field_name('rmtext'); ?>" type="text" value="<?php echo $rmtext; ?>" />
  </label>
 </p>
 <p>
  <label for="<?php echo $this->get_field_id('style'); ?>">
  <?php _e('Here you can finally style the widget. Simply type something like<br /><strong>border: 2px solid;<br />border-color: #cccccc;<br />padding: 10px;</strong><br />to get just a gray outline and a padding of 10 px. If you leave that section empty, your theme will style the widget.', 'postfeature'); ?>
- <textarea class="widefat expand<?php echo $style_height; ?>-1000" id="<?php echo $this->get_field_id('style'); ?>" name="<?php echo $this->get_field_name('style'); ?>"><?php echo $style; ?></textarea>
+ <textarea class="widefat" id="<?php echo $this->get_field_id('style'); ?>" name="<?php echo $this->get_field_name('style'); ?>"><?php echo $style; ?></textarea>
  </label>
 </p>
+<script type="text/javascript"><!--
+jQuery(document).ready(function() {
+	jQuery("#<?php echo $this->get_field_id('excerpt'); ?>").autoResize();
+});
+jQuery(document).ready(function() {
+	jQuery("#<?php echo $this->get_field_id('style'); ?>").autoResize();
+});
+--></script>
 <?php
  }
  
@@ -172,12 +201,17 @@ function update($new_instance, $old_instance) {
 	 
 	 $instance['title'] = strip_tags($new_instance['title']);
 	 $instance['article'] = strip_tags($new_instance['article']);
-	 $instance['backup'] = strip_tags($new_instance['backup']);	 
+	 $instance['backup'] = strip_tags($new_instance['backup']);
+	 $instance['class'] = strip_tags($new_instance['class']);
+	 $instance['myclass'] = strip_tags($new_instance['myclass']);
 	 $instance['thumb'] = strip_tags($new_instance['thumb']);	 
 	 $instance['image'] = strip_tags($new_instance['image']);	 
 	 $instance['width'] = strip_tags($new_instance['width']);	 
 	 $instance['subtitle'] = strip_tags($new_instance['subtitle']);
 	 $instance['excerpt'] = strip_tags($new_instance['excerpt']);
+	 $instance['noshorts'] = strip_tags($new_instance['noshorts']);
+	 $instance['readmore'] = strip_tags($new_instance['readmore']);
+	 $instance['rmtext'] = strip_tags($new_instance['rmtext']);
 	 $instance['style'] = strip_tags($new_instance['style']);
 
 	 return $instance;
@@ -218,9 +252,9 @@ function widget($args, $instance) {
 	$fpw_post_id = get_post($instance['article']);
 	$fpw_post_name = $fpw_post_id->post_name;
 	
-	if ($instance['article'] == $wp_query->get( 'p' ) || $fpw_post_name == $wp_query->get ( 'name' )) { $fpw_post = 'p='.$instance['backup']; }
+	$fpw_post = ($instance['article'] == $wp_query->get( 'p' ) || $fpw_post_name == $wp_query->get ( 'name' )) ? 'p='.$instance['backup'] : 'p='.$instance['article'];
 	
-	else { $fpw_post = 'p='.$instance['article']; }
+	if ($fpw_post=='p=') $fpw_post = 'numberposts=1&orderby=rand';
  
 /* This is the actual function of the plugin, it fills the widget with the customized post */
 
@@ -235,34 +269,48 @@ function widget($args, $instance) {
    if (!$instance['subtitle']) {
 	   
 	   ?>
-       <p>
+       <p<?php if ($instance['class']) echo ' class="'.$instance['myclass'].'"'; ?>>
        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
        </p>
 	   <?php 
    
    }
    
-   // thumbnail, if wanted
-   
-   if ($instance['image']) {
-	   
-	   $fpw_thumb = '';
-	   
-	   $fpw_image = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
-	   $fpw_thumb = $matches [1] [0];
-	   
-	   if ($fpw_thumb) {	   
-	   
-	   $fpw_image_title=$post->get_the_title;
-	   $fpw_size=getimagesize($fpw_thumb);
-	   $fpw_x=$instance['width'];
-	   $fpw_y=intval($fpw_size[1]/($fpw_size[0]/$fpw_x));
-	   
-	   ?>
-       <a href="<?php the_permalink(); ?>">
-	   <?php echo "<img title=\"".$fpw_image_title."\" src=\"".$fpw_thumb."\" alt=\"".$fpw_image_title."\" width=\"".$fpw_x."\" height=\"".$fpw_y."\" />"; ?>
-       </a>
-	   <?php
+  // thumbnail, if wanted
+
+  if ($instance['image']) {
+	  
+	  $fpw_thumb = '';
+	  $fpw_image = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+	  $fpw_thumb = $matches [1] [0];
+	  
+	  if ($fpw_thumb) {
+		  $host_string = 'http://' . $_SERVER['HTTP_HOST'];
+		  $fpw_image_title = $post->get_the_title;
+		  $fpw_x = $instance['width'];
+		  
+		  if (strncmp($fpw_thumb, $host_string, strlen($host_string)) == 0) {
+			  $realfilepath = $_SERVER['DOCUMENT_ROOT'] . urldecode(substr($fpw_thumb, strlen($host_string)));
+		  }
+		  
+		  else {
+			  $realfilepath = $fpw_thumb;
+		  }
+		  
+		  if (file_exists($realfilepath)) {
+			  $fpw_size = getimagesize($realfilepath);
+			  
+			  if (!empty($fpw_x)) $fpw_y = intval($fpw_size[1] / ($fpw_size[0] / $fpw_x));  
+		  }
+		  
+		  ?>
+          <a href="<?php the_permalink(); ?>">
+          <?php
+		  	$image_height = isset($fpw_y) ? ' height="' . $fpw_y . '"' : '';
+			$image_width = ' width="' . $fpw_x . '"';
+			
+			echo "<img title=\"" . $fpw_image_title . "\" src=\"" . $fpw_thumb . "\" alt=\"" . $fpw_image_title . "\"" . $image_width . $image_height . " />";
+		?></a><?php
 	
    }}
    
@@ -287,7 +335,7 @@ function widget($args, $instance) {
 	if ($instance['subtitle']) {
 	   
 	   ?>
-       <p>
+       <p<?php if ($instance['class']) echo ' class="'.$instance['myclass'].'"'; ?>>
        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
        </p>
 	   <?php 
@@ -305,11 +353,13 @@ function widget($args, $instance) {
 		
 	}
 	
-/* in case the excerpt is not definded by theme or anything else, the first 3 sentences of the content are given */
+/* in case the excerpt is not definded by the theme or anything else, the first 3 sentences of the content are given */
 	
 	if (!$fpw_excerpt) {
 		
 		$fpw_text=preg_replace('/\[caption(.*?)\[\/caption\]/', '', get_the_content());
+																					
+		if ($instance['noshorts']) $fpw_text=preg_replace('#\[(.*?)\]#', '', $fpw_text);
 		
 		$fpw_short=array_slice(preg_split("/([\t.!?]+)/", $fpw_text, -1, PREG_SPLIT_DELIM_CAPTURE), 0, 6);
 			
@@ -317,7 +367,19 @@ function widget($args, $instance) {
 	
 	}
 	
-	echo "<p>".$fpw_excerpt."</p>";
+/* do we want the read more link and do we have text for it? */
+
+	if ($instance['readmore']) {
+		
+		$fpw_rmtext=$instance['rmtext'];
+		
+		if (!$fpw_rmtext) $fpw_rmtext='[...]';
+		
+		$fpw_excerpt.=' <a href="'.get_permalink().'">'.$fpw_rmtext.'</a>';
+		
+	}
+		
+	echo '<p>'.do_shortcode($fpw_excerpt).'</p>';
 	
 	
 	endforeach;
